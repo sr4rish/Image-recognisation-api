ae63c76a238bc2a56651a6c35282efa0431b3eee
Sourav Rishishwar <rishishwar.sourav@gmail.com>
2020-05-24 12:42:55 +0530

    adding port

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) =>{
-    res.json(database.users)
+    res.json("It is working")
 })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
@@ -37,6 +37,6 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImagePut(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, ()=>{
-    console.log('app running on port 3000')
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('app running on port ${process.env.PORT}')
 })
