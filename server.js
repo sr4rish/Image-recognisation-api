266007ef61e1b4d36d8c03f7268a6e03e53bba1e
Sourav Rishishwar <rishishwar.sourav@gmail.com>
2020-05-24 17:26:30 +0530

    database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-cubic-44124',
-      user : 'postgres',
-      password : "asdfghjkl;'",
-      database : 'imagerecognise'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
